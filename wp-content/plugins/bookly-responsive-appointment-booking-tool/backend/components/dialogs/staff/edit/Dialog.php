b7cfb14b0fe71f6bfdfacad65d6cff44a98af750
Chloé BOIVIN <229770123+c-boivin@users.noreply.github.com>
2026-02-19 09:24:56 +0100

    Update Dialog.php : unlock the New Professional button

--- wp-content/plugins/bookly-responsive-appointment-booking-tool/backend/components/dialogs/staff/edit/Dialog.php
+++ wp-content/plugins/bookly-responsive-appointment-booking-tool/backend/components/dialogs/staff/edit/Dialog.php
@@ -78,7 +78,7 @@ class Dialog extends Lib\Base\Component
             'createStaff' => esc_attr__( 'Create staff', 'bookly' ),
             'currentTab' => self::parameter( 'tab', 'details' ),
             'editStaff' => esc_attr__( 'Edit staff', 'bookly' ),
-            'proRequired' => (int) ! Lib\Config::proActive(),
+            'proRequired' => 0, // (int) ! Lib\Config::proActive(),
             'settingsSaved' => esc_attr__( 'Settings saved.', 'bookly' ),
             'staff' => $query->sortBy( 'position' )->fetchArray(),
         ) );
